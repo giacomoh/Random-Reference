7901033e75d7ec847471090f6ef68daa1579b89f
giacomoh <53836108+giacomoh@users.noreply.github.com>
2024-04-05 04:10:23 +0200

    Update refPC.cpp

--- refPC.cpp
+++ refPC.cpp
@@ -16,11 +16,13 @@
 
 int main(int argc, char* argv[])
 {
-
     QApplication app(argc, argv);
 
     // Set the application name
-    app.setApplicationName("RandomReferencePicker");
+    app.setApplicationName("RandomReference");
+
+    // Set the global stylesheet
+    app.setStyleSheet("QPushButton { background-color: #d3d3d3; color: #000000; border-radius: 5px; padding: 5px; } QPushButton:hover { background-color: #3d3d3d; }");
 
     MainWindow mainWindow;
     mainWindow.show();
